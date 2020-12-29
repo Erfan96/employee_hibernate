b8e644b09511c0fe9d000d3152b3b2ba90504296
efsam <E.salimi75@ymail.com>
2020-12-29 13:08:38 +0330

    commit Employee.java in secondary

---
+++ src/main/java/entities/Employee.java
@@ -0,0 +1,25 @@
+package entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
+@Setter @Getter
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "emp_name", nullable = false, length = 40)
+    private String name;
+
+    @Column(name = "emp_code", nullable = false, length = 50)
+    private String empCode;
+
+    @Column(name = "salary", nullable = false)
+    private Double salary;
+}

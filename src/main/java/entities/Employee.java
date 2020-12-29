c2c3086f0d78b0ee338621f7d11257e818df4473
efsam <E.salimi75@ymail.com>
2020-12-29 13:26:47 +0330

    commit Employee.java in secondary

--- src/main/java/entities/Employee.java
+++ src/main/java/entities/Employee.java
@@ -1,13 +1,13 @@
 package entities;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import javax.persistence.*;
 
 @Entity
 @Table(name = "employee")
 @Setter @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Employee {
 

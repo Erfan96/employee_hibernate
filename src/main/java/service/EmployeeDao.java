e472379439ef0570771878d3b2890b269ade6365
efsam <E.salimi75@ymail.com>
2020-12-30 10:05:02 +0330

    commit EmployeeDao.java in secondary

--- src/main/java/service/EmployeeDao.java
+++ src/main/java/service/EmployeeDao.java
@@ -2,8 +2,12 @@ package service;
 
 import entities.Employee;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 public class EmployeeDao extends EntityDao<Employee, Integer> {
+
+    private static final String JPQL_MaxSalary = "select max(emp.salary) from Employee emp";
+    private static final String JPQL_EmployeeHasMaxSalary = "";
 
     public EmployeeDao(EntityManager entityManager) {
         super(entityManager);
@@ -11,5 +15,15 @@ public class EmployeeDao extends EntityDao<Employee, Integer> {
 
     public Class<Employee> getEntityClass() {
         return Employee.class;
+    }
+
+    public Double getMaxSalary() {
+        TypedQuery<Double> query = super.entityManager.createQuery(JPQL_MaxSalary, Double.class);
+        return query.getSingleResult();
+    }
+
+    public Employee getEmployeeHasMaxSalary() {
+        TypedQuery<Employee> typedQuery = super.entityManager.createQuery(, Employee.class);
+        return typedQuery.getSingleResult();
     }
 }

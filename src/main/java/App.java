48c055b74d46a912e81861cb56148e1a1d672267
efsam <E.salimi75@ymail.com>
2021-01-08 13:04:35 +0330

    commit App.java in secondary

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,45 +19,45 @@ public class App {
         entityManager.getTransaction().begin();
 
 //        Employee employee = new Employee();
-//        employee.setName("Hasan");
-//        employee.setEmpCode("e2");
-//        employee.setSalary(400.0);
+//        employee.setName("Shahram");
+//        employee.setEmpCode("e4");
+//        employee.setSalary(500.0);
 //
 //        employeeDao.save(employee);
 //
 //        Address address1 = new Address();
-//        address1.setPostalCode("11-2225-6578");
-//        address1.setPostalAddress("kh miri- k farzin - pelak 10");
-//        address1.setCity("tehran");
+//        address1.setPostalCode("49-7364-1982");
+//        address1.setPostalAddress("kh akrami- k lesani - pelak 753");
+//        address1.setCity("kerman");
 //        address1.setEmployee(employee);
 //
 //        addressDao.save(address1);
 //
 //        Address address2 = new Address();
 //        address2.setPostalCode("25-4561-78923");
-//        address2.setPostalAddress("kh Amir- k kazemi - pelak 91.1");
-//        address2.setCity("tehran");
+//        address2.setPostalAddress("kh shiri- k hakem - pelak 22");
+//        address2.setCity("kerman");
 //        address2.setEmployee(employee);
 //
 //        addressDao.save(address2);
 //
 //        PhoneNumber phoneNumber1 = new PhoneNumber();
 //        phoneNumber1.setAddress(address1);
-//        phoneNumber1.setMobNumber("09124569875");
-//        phoneNumber1.setTelNumber("02166609851");
+//        phoneNumber1.setMobNumber("09174569875");
+//        phoneNumber1.setTelNumber("09176564555");
 //
 //        phoneNumberDao.save(phoneNumber1);
 //
 //        PhoneNumber phoneNumber2 = new PhoneNumber();
 //        phoneNumber2.setAddress(address1);
-//        phoneNumber2.setMobNumber("09124791297");
-//        phoneNumber2.setTelNumber("02155507139");
+//        phoneNumber2.setMobNumber("09172226644");
+//        phoneNumber2.setTelNumber("09173456789");
 //
 //        phoneNumberDao.save(phoneNumber2);
 
 //        Address address = addressDao.load(5);
 //        System.out.println(address.getPhoneNumbers());
-
+//
 //        Employee employee = entityManager.find(Employee.class, 3);
 //        System.out.println(employee.getName());
 //        entityManager.remove(employee);
@@ -66,8 +66,17 @@ public class App {
         Double aDouble = employeeDao.getMaxSalary();
         System.out.println(aDouble);
 
-        Employee employee = employeeDao.getEmployeeHasMaxSalary();
-        System.out.println(employee);
+        Employee employeeA = employeeDao.getEmployeeHasMaxSalary();
+        System.out.println(employeeA);
+
+
+        employeeDao.getMaxSalaryPerCity();
+
+        employeeDao.getEmployeeHasMaxSalaryPerCity();
+
+        employeeDao.getEmployeeWithPostalCode("54-9874-2594");
+
+        employeeDao.getEmployeeWithTelNumber("09173456789");
 
         entityManager.getTransaction().commit();
         entityManager.close();
